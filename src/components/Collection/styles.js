b43d1f382569cc4e943f3899a2298e02aed7ea10
David Vargas <53392899+devDrodva@users.noreply.github.com>
2021-01-15 11:58:13 -0600

    features add button (#8)
    features add button

--- src/components/Collection/styles.js
+++ src/components/Collection/styles.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
 const StyledCollection = styled.div`
-  width: 100%;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(230px, 1fr));
-  gap: 25px;
+  gap: 20px;
+  width: 100%;
 `
 export default StyledCollection
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components'
 
 export const StyledNav = styled.nav`
-  width: 100%;
-  height: 50px;
-  margin: 0 auto;
-  padding: 0 200px;
   display: Flex;
   align-content: center;
   justify-content: space-between;
+  height: 50px;
+  padding: 0 200px;
   background-color: #fff;
 `
 
