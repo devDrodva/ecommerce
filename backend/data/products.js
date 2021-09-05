84c49616780c346fda572ad5abb008e352f0c10b
devDrodva <dvargas0720@gmail.com>
2021-09-04 23:47:21 -0700

    first commit :wrench:

---
+++ backend/data/products.js
@@ -0,0 +1,102 @@
+const products = [
+    {
+        name: 'Red Sweater',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Winter-Fabric-Textile-Fashion-Knitting-Clothing-3831821.jpg',
+        price: 25,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Black Hoodie',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Clothing-Fabric-Winter-Knitting-Textile-Fashion-3831814.jpg',
+        price: 20,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Formal Red Shirt',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Fashion-Woman-Model-Textile-Fabric-Clothes-Winter-3847131.jpg',
+        price: 80,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Dark Green Blouse',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Girl-Woman-People-Fashion-Kazakh-Clothes-Adult-3902614.jpg',
+        price: 80,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Dark Jean',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Fashion-Textile-Clothes-Current-Season-Daily-White-4514915.jpg',
+        price: 45,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Black Pant',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Daily-Fashion-Textile-Clothes-White-Current-Season-4514891.jpg',
+        price: 135,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'White Squares Pant',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Textile-White-Current-Season-Daily-Fashion-Clothes-4514890.jpg',
+        price: 20,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Red Pant',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Fashion-White-Daily-Textile-Current-Season-Clothes-4514911.jpg',
+        price: 20,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Dark Suit',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Male-Man-Clothing-Fashion-Person-People-Shirt-3737405.jpg',
+        price: 325,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Formal Red Shirt',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Person-Man-Fashion-Clothing-People-Shirt-Male-3737408.jpg',
+        price: 20,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Dark Jeans',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Fabric-Fashion-Pants-Shoes-Denim-Clothing-3737416.jpg',
+        price: 25,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Light Suit',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Fashion-Shirt-Person-Clothing-People-Male-Man-3737407.jpg',
+        price: 25,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Green Shirt',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Clothing-Textile-Fabric-Winter-Fashion-Knitting-3831834.jpg',
+        price: 40,
+        countInStock: 30,
+        description: 'red'
+    },
+    {
+        name: 'Dark Jeans',
+        imageUrl: 'https://www.maxpixel.net/static/photo/2x/Daily-White-Textile-Fashion-Clothes-Current-Season-4514888.jpg',
+        price: 25,
+        countInStock: 30,
+        description: 'red'
+    },
+]
+
+module.exports = products
